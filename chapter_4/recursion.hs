87fb1f3fa7b8c1087a63a7697592572c2d983415
Miles Disch <milesdisch@Miless-MacBook-Air.local>
2016-08-19 09:01:53 +1000

    Curried functions.

--- chapter_4/recursion.hs
+++ chapter_4/recursion.hs
@@ -66,3 +66,20 @@ divideByTen' x = (/) x 10
 divideByTen'' :: (Floating a) => a -> a  
 divideByTen'' = (/) 10 
 
+isUpperAlphanum :: Char -> Bool  
+isUpperAlphanum = (`elem` ['A'..'Z'])  
+
+isUpperAlphanum' :: Char -> Bool  
+isUpperAlphanum' = (`elem` ['A'..'Z'])  
+
+isSetOfTen :: Int -> Bool
+isSetOfTen x = x `elem` [1..10]
+
+isSetOfTen' :: Int -> Bool
+isSetOfTen' = (`elem` [1..10])
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]  
+zipWith' _ [] _ = []  
+zipWith' _ _ [] = []  
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys  
+
