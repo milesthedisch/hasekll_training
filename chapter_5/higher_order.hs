c2e14c0be003f6750ca6e6f5afe6e1d5f8c2f5c2
Miles Disch <milesdisch@Miless-MacBook-Air.local>
2016-08-26 16:50:50 +1000

    Doing something.

--- chapter_5/higher_order.hs
+++ chapter_5/higher_order.hs
@@ -1,3 +1,52 @@
+maximum' :: (Num a, Ord a) => [a] -> a
+maximum' [] = error "Empty list"
+maximum' [x] = x
+maximum' (x:xs)   
+    | x > maxTail = x  
+    | otherwise = maxTail  
+    where maxTail = maximum' xs
+
+maximum'' :: (Num a, Ord a) => [a] -> a
+maximum'' [] = error "Empty list"
+maximum'' [x] = x
+maximum'' (x:xs) = max x (maximum'' xs)
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0 = []
+    | otherwise = x:replicate' (n-1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+-- This tell us that we should ignore the second parameter for this case.
+take' n _ 
+    | n <= 0   = []
+-- This tells us that we should ignore the first paramter for this case.
+take' _ []     = []
+-- Every time we recurse down we minus n to result in a edge condition later on.
+take' n (x:xs) = x : take' (n-1) xs
+
+-- Reverse a list.
+reverse' :: [a] -> [a]
+-- The edge case condition is a empty array.
+reverse' [] = []
+-- Pattern match the list and split it into x : xs : []
+-- Then reverse 
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False
+elem' a (x:xs) 
+    | a == x = True
+    | otherwise = a `elem'` xs
+
 -- Haskell knows each parameter.
 -- Each function call that dosent finish its parameters
 -- will return "curry" the function.
@@ -94,6 +143,15 @@ chain' n
 numLongChains :: Int 
 numLongChains = length (filter' (\xs -> length xs > 15) (map chain' [1..100]))
 
+-- This is same as the last functoin except we're not using lamdas
 numLongChains' :: Int
 numLongChains' = length (filter' isLongThan15 (map chain' [1..100]))
   where isLongThan15 xs = length xs > 15
+
+
+-- This makes no sense to me to write it this way.
+-- But this is a way to write it.
+-- Why you so confuse haskell :^/O
+flip' :: (a -> b -> c) -> b -> a -> c  
+flip' f = \x y -> f y x 
+
